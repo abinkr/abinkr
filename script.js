831708538f4c50bc11812b3f3bdf555ea5873ef2
ABIN K R <akr2647@gmail.com>
2023-07-24 13:36:08 +0530

    I am finished profile area here

---
+++ script.js
@@ -0,0 +1,64 @@
+ 
+var typed = new Typed('.sec-text', {
+    strings: ["ABIN K R", "Web Developer.", "Designer."],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true
+  });
+
+  function mapButton()
+  {
+    window.location.href = "https://www.google.com/maps";
+  }
+
+  function instagram()
+  {
+      window.location.href = "https://www.instagram.com/akr56___/";
+  }
+ 
+  function linkedin()
+  {
+      window.location.href = "https://www.linkedin.com/in/abin-k-r-879695247/";
+  }
+  function threads()
+  {
+      window.location.href = "https://www.threads.net/@akr56___";
+  }
+  function dribbble()
+  {
+      window.location.href = "https://dribbble.com/akr56___";
+  }
+  function email()
+  {
+    var email = 'akr2647@gmail.com';
+    var subject = 'subject';
+    var emailBody = 'Text Here';
+    document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
+  }
+  function behance()
+  {
+      window.location.href = " https://www.behance.net/abinkr";
+  }
+  function figma()
+  {
+      window.location.href = " https://www.figma.com/files/user/1257688088860980738?fuid=1257688088860980738";
+  }
+  function gumroad()
+  {
+      window.location.href = " https://akr2647.gumroad.com/?_gl=1*1r1l80b*_ga*NzY2NjQ0OTM1LjE2OTAxODE5OTg.*_ga_6LJN6D94N6*MTY5MDE4MjAwNS4xLjEuMTY5MDE4MjA1My4wLjAuMA..";
+  }
+  function youtube()
+  {
+      window.location.href = " https://youtube.com/@music56_";
+  }
+  function discord()
+  {
+      window.location.href = " https://discord.gg/JvrQx3HY";
+  }
+  function spotify()
+  {
+      window.location.href = " https://open.spotify.com/user/esh5aqwilv2r6pbutl1tx6ume";
+  }
+
+
+ 
